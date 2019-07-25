6c5ad4cee7e8facbbc008c51e3c96faf367fa242
babbulubana <46139362+babbulubana@users.noreply.github.com>
2019-07-24 23:11:11 -0700

    Updates

---
+++ precache-manifest.a641b91891ce8e7d2833f0a58d48a82c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3484074bcea9b0d1f8450e0f712155",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "a809a3695238037c5b65",
+    "url": "/mywebsite/static/css/2.606264ab.chunk.css"
+  },
+  {
+    "revision": "0da0f2515d7606c9c102",
+    "url": "/mywebsite/static/css/main.05c9279c.chunk.css"
+  },
+  {
+    "revision": "a809a3695238037c5b65",
+    "url": "/mywebsite/static/js/2.82a21dc8.chunk.js"
+  },
+  {
+    "revision": "0da0f2515d7606c9c102",
+    "url": "/mywebsite/static/js/main.5fb1203a.chunk.js"
+  },
+  {
+    "revision": "1029a0e86ffa031e1963",
+    "url": "/mywebsite/static/js/runtime~main.8e5ad122.js"
+  },
+  {
+    "revision": "37cfc9a7edffc698cef2233b4c7678f8",
+    "url": "/mywebsite/static/media/profile2.37cfc9a7.jpg"
+  },
+  {
+    "revision": "8649ad1c7ca788d73c6b80b84278096e",
+    "url": "/mywebsite/static/media/sea.8649ad1c.jpeg"
+  }
+]);
